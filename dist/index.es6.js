232f704682eb7bfaa5cc9ab7f5d5d7b8083d637a
Evgeny Ageev <makushatnik@yandex.ru>
2017-03-12 19:26:17 +0300

    Express simple app Init

---
+++ dist/index.es6.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+var app = express();
+app.use(cors());
+app.get('/', function (req, res) {
+  res.json({
+    hello: 'JS World'
+  });
+});
+app.get('/task1', function (req, res) {
+  var sum = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(sum.toString());
+});
+
+app.listen(3000, function () {
+  console.log('Your app listening on port 3000!');
+});
+//# sourceMappingURL=index.es6.js.map
